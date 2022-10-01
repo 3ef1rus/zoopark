641e5c090b3d106c40e4cd0103a4610c36571027
3ef1rus <10kmmrza10k@mail.ru>
2022-10-02 00:34:37 +0300

    first commit

---
+++ CREATE TABLE zooparkForTrade.sql
@@ -0,0 +1,9 @@
+CREATE TABLE zooparkForTrade
+(
+    zooparkID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    Name varchar(100) NOT NULL ,
+    Adress varchar (100) NOT NULL,
+    Nubmer varchar (50) NOT NULL
+    -- specify more columns here
+);
+GO
