3756f207bd9a7471613555f1f5f6aa294a4bcd82
3ef1rus <10kmmrza10k@mail.ru>
2022-10-02 14:22:22 +0300

    complete create tables

--- CREATE TABLE zooparkForTrade.sql
+++ CREATE TABLE zooparkForTrade.sql
@@ -1,13 +1,15 @@
 CREATE TABLE zooparkForTrade
 (
-    zooparkID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    zooparkID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    -- primary key column
     Name varchar(100) NOT NULL ,
     Adress varchar (100) NOT NULL,
     Number varchar (50) NOT NULL
 );
 CREATE TABLE Exchanges
 (
-    exchangeID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    exchangeID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    -- primary key column
     [Whom give] varchar(100) NOT NULL ,
     [Whom take] varchar (100) NOT NULL,
     [Date exchange] varchar (50) NOT NULL,
@@ -16,7 +18,8 @@ CREATE TABLE Exchanges
 );
 CREATE TABLE Mainzoopark
 (
-    zooparkID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    zooparkID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    -- primary key column
     [Adress] varchar(100) NOT NULL ,
     [Name] varchar (100) NOT NULL,
     [Cells] varchar (100) NOT NULL,
@@ -26,35 +29,39 @@ CREATE TABLE Mainzoopark
 );
 CREATE TABLE Animal
 (
-    animalID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    animalID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    -- primary key column
     [Weigth] varchar(100) NOT NULL ,
     [Hight] varchar (100) NOT NULL,
     [Date join] varchar (100) NOT NULL,
     [Warm] boolean NOT NULL,
     [Move] boolean NOT NULL,
-    exchangeID INT REFERENCES Exchanges (exchangeID)
-    exchangeID INT REFERENCES Exchanges (exchangeID)
+    cellID INT REFERENCES Cells (cellID),
+    passportID INT REFERENCES PassportAnimal (passportAnimaID)
 );
 CREATE TABLE Employee
 (
-    employeeID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    employeeID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    -- primary key column
     [Salarity] varchar(100) NOT NULL ,
     [Sex] varchar (100) NOT NULL,
     [Age] varchar (100) NOT NULL,
     [Experience] varchar (100) NOT NULL,
     [Full name] varchar (100) NOT NULL,
-    exchangeID INT REFERENCES Exchanges (exchangeID)
-    exchangeID INT REFERENCES Exchanges (exchangeID)
-    exchangeID INT REFERENCES Exchanges (exchangeID)
+    jobID INT REFERENCES Jobs (jobID),
+    responsibilityID INT REFERENCES Responsibility (responsibilityID),
+    interactionTypeID INT REFERENCES InteractionTypes (interactionTypeID)
 );
 CREATE TABLE Jobs
 (
-    jobID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    jobID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    -- primary key column
     [Name] varchar(100) NOT NULL
 );
 CREATE TABLE Cells
 (
-    cellID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    cellID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    -- primary key column
     [Number cell] varchar(100) NOT NULL,
     [Number animal] varchar(100) NOT NULL,
     [Warm] boolean NOT NULL,
@@ -62,74 +69,92 @@ CREATE TABLE Cells
 );
 CREATE TABLE Responsibility
 (
-    responsibilityID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    responsibilityID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    -- primary key column
     [Date start] varchar(100) NOT NULL,
     [Date end] varchar(100) NOT NULL,
     [Warm] boolean NOT NULL,
-    exchangeID INT REFERENCES Exchanges (exchangeID)-
+    passportAnimaID INT REFERENCES PassportAnimal (passportAnimaID)
 );
 CREATE TABLE InteractionTypes
 (
-    interactionTypeID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    interactionTypeID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    -- primary key column
     [InteractionTypes] varchar(100) NOT NULL
 );
 CREATE TABLE PassportAnimal
 (
-    passportAnimaID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    passportAnimaID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    -- primary key column
     [Name] varchar(100) NOT NULL,
     [Sex] varchar(100) NOT NULL,
     [Parents] varchar(100) NOT NULL,
     [DataBirth] varchar(100) NOT NULL,
     [Childs] varchar(100) NOT NULL,
     [Number passport] varchar(100) NOT NULL,
-    exchangeID INT REFERENCES Exchanges (exchangeID)-
-    exchangeID INT REFERENCES Exchanges (exchangeID)-
+    MedicalCardID INT REFERENCES MedicalCard (MedicalCardID),
+    exchangeID INT REFERENCES Exchanges (exchangeID)
+    -
 );
-CREATE TABLE MedicalCard
-(
-    MedicalCardID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
-    [Сompatibility] varchar(100) NOT NULL,
-    [Number Medical Card] varchar(100) NOT NULL,
-    exchangeID INT REFERENCES Exchanges (exchangeID)-
-    exchangeID INT REFERENCES Exchanges (exchangeID)-
-);
-CREATE TABLE Vaccinations
-(
-    vaccinationID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
-    [Name] varchar(100) NOT NULL,
-    [Date] varchar(100) NOT NULL
-);
-CREATE TABLE Disease
-(
-    diseaseID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
-    [Name] varchar(100) NOT NULL,
-    [Date start] varchar(100) NOT NULL,
-    [Date end] varchar(100) NOT NULL
-);
-CREATE TABLE Types
-(
-    MedicalCardID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
-    [Сompatibility] varchar(100) NOT NULL,
-    [Special] varchar(100) NOT NULL,
-    [Name type] varchar(100) NOT NULL,
-    [Family] varchar(100) NOT NULL,
-    exchangeID INT REFERENCES Exchanges (exchangeID)-
-);
-CREATE TABLE FoodTypes
-(
-    foodTypeID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
-    [Food type] varchar(100) NOT NULL,
-    exchangeID INT REFERENCES Exchanges (exchangeID)-
-);
-CREATE TABLE Feeds
-(
-    feedID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
-    [Name] varchar(100) NOT NULL,
-    [Amount] varchar(100) NOT NULL,
-    [Food type] varchar(100) NOT NULL,
-    [Price] varchar(100) NOT NULL,
-    [Date receiving] varchar(100) NOT NULL,
-    exchangeID INT REFERENCES Exchanges (exchangeID)-
-    exchangeID INT REFERENCES Exchanges (exchangeID)-
-);
+    CREATE TABLE MedicalCard
+    (
+        medicalCardID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+        -- primary key column
+        [Сompatibility] varchar(100) NOT NULL,
+        [Number Medical Card] varchar(100) NOT NULL,
+        vaccinationID INT REFERENCES Vaccinations (vaccinationID),
+        diseaseID INT REFERENCES Disease (diseaseID)
+    );
+    CREATE TABLE Vaccinations
+    (
+        vaccinationID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+        -- primary key column
+        [Name] varchar(100) NOT NULL,
+        [Date] varchar(100) NOT NULL
+    );
+    CREATE TABLE Disease
+    (
+        diseaseID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+        -- primary key column
+        [Name] varchar(100) NOT NULL,
+        [Date start] varchar(100) NOT NULL,
+        [Date end] varchar(100) NOT NULL
+    );
+    CREATE TABLE Types
+    (
+        typeID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+        -- primary key column
+        [Сompatibility] varchar(100) NOT NULL,
+        [Special] varchar(100) NOT NULL,
+        [Name type] varchar(100) NOT NULL,
+        [Family] varchar(100) NOT NULL,
+        foodTypeID INT REFERENCES FoodTypes (foodTypeID)
+    );
+    CREATE TABLE FoodTypes
+    (
+        foodTypeID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+        -- primary key column
+        [Food type] varchar(100) NOT NULL,
+        feedID INT REFERENCES Feeds (feedID)
+    );
+    CREATE TABLE Feeds
+    (
+        feedID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+        -- primary key column
+        [Name] varchar(100) NOT NULL,
+        [Amount] varchar(100) NOT NULL,
+        [Need] boolean NOT NULL,
+        [Date receiving] varchar(100) NOT NULL,
+        supplierID INT REFERENCES Suppliers (supplierID)
+    );
+    CREATE TABLE Suppliers
+    (
+        supplierID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+        -- primary key column
+        [Name] varchar(100) NOT NULL,
+        [Type feed] varchar(100) NOT NULL,
+        [Delivery period] boolean NOT NULL,
+        [Amount] varchar(100) NOT NULL,
+        [Price] varchar(100) NOT NULL
+    );
 GO
