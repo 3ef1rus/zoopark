4c41e976ff984a4f1a25906011c3613932628ded
3ef1rus <10kmmrza10k@mail.ru>
2022-10-02 13:51:09 +0300

    create three new tables

--- CREATE TABLE zooparkForTrade.sql
+++ CREATE TABLE zooparkForTrade.sql
@@ -47,6 +47,62 @@ CREATE TABLE Employee
     exchangeID INT REFERENCES Exchanges (exchangeID)
     exchangeID INT REFERENCES Exchanges (exchangeID)
 );
+CREATE TABLE Jobs
+(
+    jobID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    [Name] varchar(100) NOT NULL
+);
+CREATE TABLE Cells
+(
+    cellID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    [Number cell] varchar(100) NOT NULL,
+    [Number animal] varchar(100) NOT NULL,
+    [Warm] boolean NOT NULL,
+    [Types animal] archar(100) NOT NULL
+);
+CREATE TABLE Responsibility
+(
+    responsibilityID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    [Data start] varchar(100) NOT NULL,
+    [Data end] varchar(100) NOT NULL,
+    [Warm] boolean NOT NULL,
+    exchangeID INT REFERENCES Exchanges (exchangeID)-
+);
+CREATE TABLE InteractionTypes
+(
+    interactionTypeID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    [InteractionTypes] varchar(100) NOT NULL
+);
+CREATE TABLE PassportAnimal
+(
+    passportAnimaID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    [Name] varchar(100) NOT NULL,
+    [Sex] varchar(100) NOT NULL,
+    [Parents] varchar(100) NOT NULL,
+    [DataBirth] varchar(100) NOT NULL,
+    [Childs] varchar(100) NOT NULL,
+    [Number passport] varchar(100) NOT NULL,
+    exchangeID INT REFERENCES Exchanges (exchangeID)-
+    exchangeID INT REFERENCES Exchanges (exchangeID)-
+);
+CREATE TABLE MedicalCard
+(
+    MedicalCardID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    [Сompatibility] varchar(100) NOT NULL,
+    [Number Medical Card] varchar(100) NOT NULL,
+    exchangeID INT REFERENCES Exchanges (exchangeID)-
+    exchangeID INT REFERENCES Exchanges (exchangeID)-
+);
+CREATE TABLE Types
+(
+    MedicalCardID INT NOT NULL PRIMARY KEY IDENTITY(1,1), -- primary key column
+    [Сompatibility] varchar(100) NOT NULL,
+    [Special] varchar(100) NOT NULL,
+    [Name type] varchar(100) NOT NULL,
+    [Family] varchar(100) NOT NULL,
+    exchangeID INT REFERENCES Exchanges (exchangeID)-
+    exchangeID INT REFERENCES Exchanges (exchangeID)-
+);
 
 
 GO
