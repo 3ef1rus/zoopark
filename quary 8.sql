18092ba7ce6b012d81727cba2c20aca2100f0043
3ef1rus <10kmmrza10k@mail.ru>
2022-10-03 07:29:29 +0300

    fix

---
+++ quary 8.sql
@@ -0,0 +1,5 @@
+use zoopark
+SELECT Suppliers.Name,Suppliers.[Type feed],Suppliers.Price,
+ Suppliers.[Delivery period], Suppliers.Amount 
+FROM Suppliers
+GO
---
+++ quary 9.sql
@@ -0,0 +1,4 @@
+use zoopark
+SELECT Suppliers.Amount,Feeds.Name FROM Suppliers,Feeds
+WHERE Suppliers.Name='Зоопарк Радость' AND 
+Suppliers.[supplierID]=Feeds.supplierID
