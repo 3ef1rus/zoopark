c4a772d7dbc9de927181a0bdd6305b153acf763a
3ef1rus <10kmmrza10k@mail.ru>
2022-10-03 07:39:50 +0300

    create quary 10

--- quary 10.sql
+++ quary 10.sql
@@ -1,1 +1,6 @@
-SELECT asdf
+use zoopark
+SELECT [Animal name]=PassportAnimal.Name ,Types.[Name type],FoodTypes.[Food type] FROM Animal,Types,PassportAnimal,FoodTypes
+WHERE FoodTypes.foodTypeID=Types.foodTypeID AND
+    Types.typeID=PassportAnimal.passportAnimaID AND
+    PassportAnimal.passportAnimaID=Animal.passportAnimaID
+
