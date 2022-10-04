2036a38a748b17acbb505f3faa111344a48676ae
3ef1rus <10kmmrza10k@mail.ru>
2022-10-04 17:13:16 +0300

    fix

--- quary 10.sql
+++ quary 10.sql
@@ -1,6 +1,4 @@
 use zoopark
-SELECT [Animal name]=PassportAnimal.Name ,Types.[Name type],FoodTypes.[Food type] FROM Animal,Types,PassportAnimal,FoodTypes
-WHERE FoodTypes.foodTypeID=Types.foodTypeID AND
-    Types.typeID=PassportAnimal.passportAnimaID AND
-    PassportAnimal.passportAnimaID=Animal.passportAnimaID
-
+SELECT Types.[Name type],FoodTypes.[Food type] FROM Types,FoodTypes
+WHERE FoodTypes.foodTypeID=Types.foodTypeID
+GO
--- quary 13.sql
+++ quary 13.sql
@@ -1,4 +1,7 @@
 use zoopark
-SELECT Exchanges.[Number exchange],Exchanges.[Date exchange],Exchanges.[Whom give],Exchanges.[Whom take] ,[Zoopark exchange]=ZooparkForTrade.Name FROM ZooparkForTrade,Exchanges
+SELECT Exchanges.[Number exchange],Exchanges.[Date exchange],
+Exchanges.[Whom give],Exchanges.[Whom take] ,
+[Zoopark exchange]=ZooparkForTrade.Name 
+FROM ZooparkForTrade,Exchanges
 WHERE Exchanges.zooparkID=ZooparkForTrade.zooparkID
 GO
