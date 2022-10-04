49ff761ebbadf006ed74e47fcbc2ac11f8c51397
3ef1rus <10kmmrza10k@mail.ru>
2022-10-04 17:06:53 +0300

    fix quary 11

--- quary 11.sql
+++ quary 11.sql
@@ -1,12 +1,20 @@
 use zoopark
-SELECT Animal.Hight,Animal.Weigth,[Vaccination Name]=Vaccinations.Name,[Disease Name]=Disease.Name,PassportAnimal.[Data Birth],Animal.[Date join],PassportAnimal.Childs
-FROM Animal,PassportAnimal,Vaccinations,VaccinationsList,MedicalCard,Disease,DiseaseList
-WHERE PassportAnimal.Name='Фиеста' AND
+SELECT Animal.Hight,Animal.Weigth,[Animal name]=PassportAnimal.Name,PassportAnimal.[Data Birth],
+Animal.[Date join],PassportAnimal.Childs
+FROM Animal,PassportAnimal,MedicalCard
+WHERE PassportAnimal.Name='Стимми' AND
 PassportAnimal.passportAnimaID=Animal.passportAnimaID AND
+PassportAnimal.MedicalCardID=MedicalCard.medicalCardID
+SELECT Vaccinations.Name as Vaccinations 
+FROM Vaccinations,VaccinationsList,MedicalCard,PassportAnimal
+WHERE   PassportAnimal.Name='Стимми' AND
 PassportAnimal.MedicalCardID=MedicalCard.medicalCardID AND
 MedicalCard.medicalCardID=VaccinationsList.idMedCard AND
-VaccinationsList.vaccinationID=Vaccinations.vaccinationID AND
+VaccinationsList.vaccinationID=Vaccinations.vaccinationID
+SELECT Disease.Name as Diseases 
+FROM Disease,DiseaseList    ,MedicalCard,PassportAnimal
+WHERE   PassportAnimal.Name='Стимми' AND
+PassportAnimal.MedicalCardID=MedicalCard.medicalCardID AND
 MedicalCard.medicalCardID=DiseaseList.idMedCard AND
 DiseaseList.diseaseID=Disease.diseaseID
-
 GO
