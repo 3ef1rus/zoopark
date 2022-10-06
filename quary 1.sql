fa8e843843f75a95afdf55be1265fbb65197af87
3ef1rus <10kmmrza10k@mail.ru>
2022-10-06 11:31:56 +0300

    fix

--- quary 1.sql
+++ quary 1.sql
@@ -1,6 +1,6 @@
 use zoopark
 SELECT Employee.[Full name],[Job]=Jobs.Name,Employee.Experience,Employee.Sex,
-Employee.Age,Employee.Salarity+"$" 
+Employee.Age,Employee.Salarity
 from Employee,Jobs
 WHERE Jobs.jobID=Employee.jobID
 GO
