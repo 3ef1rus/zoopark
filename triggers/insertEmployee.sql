a33a7a7a46d32c02b238665fda64356c8cff8332
3ef1rus <10kmmrza10k@mail.ru>
2022-10-30 18:07:05 +0300

    create trigger delete info

---
+++ triggers/insertEmployee.sql
@@ -0,0 +1,6 @@
+use zoopark;
+
+INSERT INTO Employee ([Full name],[Salarity],[Sex],[Age],[Experience],[jobID])
+VALUES('Домодедов Степан Игоревич', '432', 'М', '49 лет','лет','4')
+ 
+SELECT * FROM EmployeeHistory
